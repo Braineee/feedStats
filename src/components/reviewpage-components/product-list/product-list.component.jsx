297c84fb41ad730963fd823796ce6f3b39099063
Braineee <alindavidsin@yahoo.com>
2020-06-28 15:38:26 +0100

    WIP

--- src/components/reviewpage-components/product-list/product-list.component.jsx
+++ src/components/reviewpage-components/product-list/product-list.component.jsx
@@ -6,7 +6,10 @@ const ProductList = () => (
   <div id="product-list">
     <div className="container">
       <div className="row row-cols-1 row-cols-lg-2">
-        
+        <div className="col">
+          <div className="product-card">
+            </div>
+        </div>
       </div>
     </div>
   </div>
